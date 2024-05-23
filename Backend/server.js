8e31acc70427c5364846e71d7914ce8c15a58f10
Ashish <78149551+Ashish0209@users.noreply.github.com>
2024-05-24 00:43:12 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './db/connectdb.js';
+import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
+import postRoutes from "./routes/postRoutes.js";
+import {v2 as cloudinary} from "cloudinary";
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+
+});
+
+app.use(express.json({limit:"50mb"}));   // parse json data in req.body
+app.use(express.urlencoded({extended:true})); // parse for data in req.body
+app.use(cookieParser());
+
+// Routes 
+
+app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`server started at port number ${PORT}`);
+})
